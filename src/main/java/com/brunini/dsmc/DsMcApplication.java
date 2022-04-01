90d3400fc13014f883d1204983b46ae1187c20f2
heitorbrunini <heitorbrunini.ifpb@gmail.com>
2022-04-01 16:25:11 -0300

    first commit

---
+++ src/main/java/com/brunini/dsmc/DsMcApplication.java
@@ -0,0 +1,13 @@
+package com.brunini.dsmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsMcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brunini/dsmc/DsMcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunini.dsmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
